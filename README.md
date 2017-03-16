282800b0cbdd309134693b220c025810a58d8dd6
Arman Anzanpour <armanz@utu.fi>
2017-03-16 18:59:27 +0200

    The code is added and the readme is updated.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # esp8266-fast-udp-client
 A code to test how fast esp8266 is when in sending continuous UDP packets.
+The code is working ESP8266 Arduino firmware.
+The results shows that the speed of UDP sending depends on:
+- The network speed.
+- The server performance.
+- The length of packet.
---
+++ esp8266-udp-client.ino
@@ -0,0 +1,89 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+//  IPAddress ip(10, 42, 0, 69); 
+//  IPAddress gateway(10, 42, 0, 255);
+//  IPAddress subnet(255, 255, 255, 0);
+
+
+const char* ssid     = "iotc";
+const char* password = "darman933";
+const char* ipd = "192.168.43.123";
+const int port = 33333;
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+   delay(10);
+   // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+//WiFi.config(ip, gateway, subnet);
+  WiFi.begin(ssid, password);
+
+
+
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(2000);
+  
+}
+
+WiFiUDP Udp;
+
+unsigned long tim=micros();
+unsigned long tic=micros();
+int packageCount = 0;
+char charBuf[755]; 
+String stringOne = "";
+int j=10000;
+
+void loop() {
+
+      j++;
+      if (j<12001) { // testing with 2000 packets
+      Serial.println(j);
+      stringOne="1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890";
+      stringOne=stringOne+"1234567890123456789012345678901234567890"; // 750 Bytes string
+      stringOne=stringOne+"1234567890123456789012345"+String(j);
+      stringOne.toCharArray(charBuf,751);
+      
+      Udp.beginPacket(ipd, port);
+      Udp.write(charBuf);
+      Udp.endPacket();  
+      
+      //delay(3); // for a 20Mbps WiFi network sending directly to the a Linode 4096 Node.js server.
+      delay(90); // for a 2Mbps mobile hotspot sending to Core i3 Node.js server.
+      
+      } 
+    
+  
+}
+
